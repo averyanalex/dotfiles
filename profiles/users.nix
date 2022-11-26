fb4277deb36fafd49c8d31b211151a7f2b7a8a80
AveryanAlex <alex@averyan.ru>
2022-11-26 19:39:10 +0300

    Rotate ssh key

--- profiles/users.nix
+++ profiles/users.nix
@@ -11,7 +11,7 @@
         extraGroups = [ "wheel" ];
         uid = 1000;
         passwordFile = config.age.secrets.password-alex.path;
-        openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINF5KDy1T6Z+RlDb+Io3g1uSZ46rhBxhNE39YlG3GPFM alex@averyan.ru" ];
+        openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDP6BYhOQG5swda8e3YRo4LqhdNNAQd3NwkQME193izZ alex@averyan.ru" ];
       };
     };
   };
