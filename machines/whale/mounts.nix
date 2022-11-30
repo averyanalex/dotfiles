6650d12f282379233fd03e8e8dc943e79cc222da
AveryanAlex <alex@averyan.ru>
2022-11-30 12:50:18 +0300

    Whale tank

--- machines/whale/mounts.nix
+++ machines/whale/mounts.nix
@@ -9,4 +9,16 @@
     device = "/dev/nvme0n1p2";
     fsType = "vfat";
   };
+
+  fileSystems."/tank" = {
+    device = "/dev/whale/tank";
+    fsType = "ext4";
+    options = [ "discard" ];
+  };
+
+  fileSystems."/tank/dwnl" = {
+    device = "/dev/whale/dwnl";
+    fsType = "ext4";
+    options = [ "discard" ];
+  };
 }
