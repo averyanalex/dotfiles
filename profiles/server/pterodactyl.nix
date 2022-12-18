fcaa88bf3bfc8bd0a07136268f69f188643bbd4a
AveryanAlex <alex@averyan.ru>
2022-12-18 20:37:45 +0300

    Setup pterodactyl

--- profiles/server/pterodactyl.nix
+++ profiles/server/pterodactyl.nix
@@ -17,8 +17,16 @@
   };
 
   systemd.services."container@pterodactyl" = {
-    wants = [ "wireguard-wg-pterodactyl.target" ];
-    after = [ "wireguard-wg-pterodactyl.target" ];
+    wants = [ "wireguard-wg-pterodactyl.target" "setup-pterodactyl-dirs.service" ];
+    after = [ "wireguard-wg-pterodactyl.target" "setup-pterodactyl-dirs.service" ];
+  };
+
+  systemd.services.setup-pterodactyl-dirs = {
+    script = ''
+      mkdir -p /persist/ptero/mysql
+      chown 84:84 /persist/ptero/mysql
+      chmod 700 /persist/ptero/mysql
+    '';
   };
 
   containers.pterodactyl = {
@@ -29,6 +37,13 @@
     interfaces = [ "wg-pterodactyl" ];
 
     extraFlags = [ "--system-call-filter=@keyring" "--system-call-filter=bpf" ];
+
+    bindMounts = {
+      "/var/lib/mysql/" = {
+        hostPath = "/persist/ptero/mysql";
+        isReadOnly = false;
+      };
+    };
 
     config = { config, pkgs, ... }: {
       system.stateVersion = "22.11";
