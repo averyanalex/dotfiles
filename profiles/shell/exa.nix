f8a39dc7667e9779be6e97880b10bd681f348b70
AveryanAlex <alex@averyan.ru>
2023-01-24 15:48:19 +0300

    Rebuild zsh aliases

--- profiles/shell/exa.nix
+++ profiles/shell/exa.nix
@@ -8,6 +8,8 @@
       ls = "exa --icons -l";
       la = "exa --icons -la";
       lt = "exa --icons --tree";
+      upd = "sudo rm -rf /root/.cache && sudo nixos-rebuild switch --flake github:averyanalex/nixcfg";
+      lupd = "sudo nixos-rebuild switch --flake .";
     };
   };
 }
