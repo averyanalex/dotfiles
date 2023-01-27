c04a533b4b685f569858ede1e4e55974f77cdcca
AveryanAlex <alex@averyan.ru>
2023-01-27 16:07:05 +0300

    Alligator AX210

--- machines/alligator/default.nix
+++ machines/alligator/default.nix
@@ -3,6 +3,8 @@
 {
   imports = [
     inputs.self.nixosModules.roles.desktop
+    inputs.self.nixosModules.profiles.bluetooth
+    inputs.self.nixosModules.profiles.netman
     inputs.self.nixosModules.profiles.openrgb
     ./hardware.nix
     ./mounts.nix
@@ -15,18 +17,18 @@
   networking = {
     firewall.allowedTCPPorts = [ 25565 ];
 
-    defaultGateway = {
-      address = "192.168.3.1";
-      interface = "enp10s0";
-    };
+    # defaultGateway = {
+    #   address = "192.168.3.1";
+    #   interface = "enp10s0";
+    # };
 
-    interfaces.enp10s0 = {
-      ipv4 = {
-        addresses = [{
-          address = "192.168.3.60";
-          prefixLength = 24;
-        }];
-      };
-    };
+    # interfaces.enp10s0 = {
+    #   ipv4 = {
+    #     addresses = [{
+    #       address = "192.168.3.60";
+    #       prefixLength = 24;
+    #     }];
+    #   };
+    # };
   };
 }
