af1931c72d62a400ea229b40de13793f1c451da8
AveryanAlex <alex@averyan.ru>
2023-03-01 22:40:11 +0300

    Hawk no hawk ygg

--- profiles/yggdrasil.nix
+++ profiles/yggdrasil.nix
@@ -1,11 +1,11 @@
-{ lib, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 {
   services.av-yggdrasil = {
     enable = true;
     persistentKeys = true;
     keysPath = "/persist/etc/yggdrasil/keys.json";
     settings = {
-      Peers = [
+      Peers = lib.mkIf (config.networking.hostName != "hawk") [
         "tls://ygg.averyan.ru:8362"
       ];
       IfName = "ygg0";
