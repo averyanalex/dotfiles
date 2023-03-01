ed31b12ddc04df035d4a6cbb2b63397d444fd5ca
AveryanAlex <alex@averyan.ru>
2023-03-01 22:42:56 +0300

    Yggdrasil public node

--- profiles/yggdrasil.nix
+++ profiles/yggdrasil.nix
@@ -5,6 +5,7 @@
     persistentKeys = true;
     keysPath = "/persist/etc/yggdrasil/keys.json";
     settings = {
+      Listen = [ "tls://[::]:8362" ];
       Peers = lib.mkIf (config.networking.hostName != "hawk") [
         "tls://ygg.averyan.ru:8362"
       ];
@@ -12,4 +13,6 @@
     };
     denyDhcpcdInterfaces = [ "ygg0" ];
   };
+
+  networking.firewall.allowedTCPPorts = [ 8362 ];
 }
