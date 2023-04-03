1c113bfb6b35a72e871ca2ad67116b1d3785f856
AveryanAlex <alex@averyan.ru>
2023-04-03 21:43:00 +0300

    Fix server config

--- roles/server.nix
+++ roles/server.nix
@@ -3,6 +3,19 @@
     ./base.nix
   ] ++ (with inputs.self.nixosModules.modules; [
   ]) ++ (with inputs.self.nixosModules.profiles; [
-    server
   ]);
+
+  systemd = {
+    enableEmergencyMode = false;
+
+    watchdog = {
+      runtimeTime = "30s";
+      rebootTime = "10m";
+    };
+
+    sleep.extraConfig = ''
+      AllowSuspend=no
+      AllowHibernation=no
+    '';
+  };
 }
