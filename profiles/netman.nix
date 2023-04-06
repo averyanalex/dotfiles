1c58251722d04bc03e18e21999e5257685ec1883
AveryanAlex <alex@averyan.ru>
2023-04-06 16:51:27 +0300

    Disable wait-online

--- profiles/netman.nix
+++ profiles/netman.nix
@@ -1,5 +1,7 @@
 {
   networking.networkmanager.enable = true;
+  systemd.services.NetworkManager-wait-online.enable = false;
+
   persist.state.dirs = [
     "/etc/NetworkManager"
     "/var/lib/NetworkManager"
