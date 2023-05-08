f71fe21ff589cfae7ca3094a1fe1be6ed0b9a863
AveryanAlex <alex@averyan.ru>
2023-05-08 14:20:05 +0300

    Improve firefox

--- profiles/apps/firefox.nix
+++ profiles/apps/firefox.nix
@@ -22,6 +22,21 @@
           isDefault = true;
           settings = {
             "media.ffmpeg.vaapi.enabled" = true;
+
+            # Disable pocket
+            "extensions.pocket.enabled" = false;
+            "browser.newtabpage.activity-stream.discoverystream.saveToPocketCard.enabled" = false;
+
+            # Block DRM
+            "media.eme.enabled" = false;
+            "media.gmp-manager.url" = "data:text/plain,";
+            "media.gmp-provider.enabled" = false;
+            "media.gmp-gmpopenh264.enabled" = false;
+
+            # New tab
+            "browser.newtabpage.activity-stream.feeds.section.topstories" = false;
+            "browser.newtabpage.activity-stream.showSponsored" = false;
+            "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
           };
         };
       };
