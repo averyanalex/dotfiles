756e3a0422f17e6238f19ec3b71dd3ce3a53c8f2
AveryanAlex <alex@averyan.ru>
2023-08-08 14:10:50 +0300

    Zenpower

--- hardware/cpu/amd.nix
+++ hardware/cpu/amd.nix
@@ -1,6 +1,8 @@
-{
+{config, ...}: {
   boot.kernelModules = ["kvm-amd"];
   hardware.enableRedistributableFirmware = true;
   hardware.cpu.amd.updateMicrocode = true;
   powerManagement.cpuFreqGovernor = "schedutil";
+
+  boot.extraModulePackages = [config.boot.kernelPackages.zenpower];
 }
