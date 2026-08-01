d5f2ee5228f9b35409ac49a9cba2d474786e1ff2
AveryanAlex <alex@averyan.ru>
2026-08-02 01:48:25 +0300

    feat(whale): add emerald append-only backup repo

--- machines/whale/borgserve.nix
+++ machines/whale/borgserve.nix
@@ -1,3 +1,4 @@
+{ pkgs, ... }:
 {
   services.borgbackup.repos.qfzwufeu = {
     quota = "150G";
@@ -7,17 +8,45 @@
     ];
   };
 
+  services.borgbackup.repos.emerald = {
+    quota = "200G";
+    path = "/var/lib/borgbackup/emerald";
+    authorizedKeysAppendOnly = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINN/1XPmuJAEJC5F2KWm/XosIF1z6zWNOE2LYpEtn/N+ emerald-backup"
+    ];
+  };
+
   fileSystems."/var/lib/borgbackup" = {
     device = "UUID=bcfa404a-68de-4a25-9fb0-4e972c8f9423";
     fsType = "btrfs";
     options = [
-      "compress=none"
+      "compress=zstd:7"
       "noatime"
       "subvol=@borgbackup"
     ];
   };
 
-  networking.firewall.extraInputRules = ''
-    ip6 saddr 200:571e:effc:1361:696b:35b5:e577:5a63 tcp dport 22 accept
-  '';
+  systemd.services = {
+    borgbackup-no-compression = {
+      description = "Disable CoW and compression for the BorgBackup subvolume";
+      requires = [ "var-lib-borgbackup.mount" ];
+      after = [ "var-lib-borgbackup.mount" ];
+      wantedBy = [ "multi-user.target" ];
+      serviceConfig = {
+        Type = "oneshot";
+        RemainAfterExit = true;
+        ExecStart = "${pkgs.e2fsprogs}/bin/chattr +C /var/lib/borgbackup";
+      };
+    };
+
+    borgbackup-repo-qfzwufeu = {
+      requires = [ "borgbackup-no-compression.service" ];
+      after = [ "borgbackup-no-compression.service" ];
+    };
+
+    borgbackup-repo-emerald = {
+      requires = [ "borgbackup-no-compression.service" ];
+      after = [ "borgbackup-no-compression.service" ];
+    };
+  };
 }
