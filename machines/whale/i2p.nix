70bbd99c3e199f78e79e6015d30265965e71a174
AveryanAlex <alex@averyan.ru>
2026-09-06 14:59:10 +0300

    fix(whale): migrate i2pd configuration to settings

--- machines/whale/i2p.nix
+++ machines/whale/i2p.nix
@@ -1,21 +1,34 @@
+{ lib, ... }:
 {
-  services.i2pd = {
-    enable = true;
-    port = 17283;
-    address = "95.165.105.90";
-
-    bandwidth = 2048;
-
-    ntcp2 = {
-      enable = true;
-      port = 15728;
-      published = true;
-    };
-
-    yggdrasil.enable = true;
+  users.users.i2pd = {
+    isSystemUser = true;
+    group = "i2pd";
+    uid = 150;
   };
 
-  systemd.services.i2pd.serviceConfig.MemoryMax = "512M";
+  users.groups.i2pd.gid = 150;
+
+  services.i2pd = {
+    enable = true;
+    settings = {
+      host = "95.165.105.90";
+      port = 17283;
+      bandwidth = 2048;
+
+      ntcp2 = {
+        enabled = true;
+        port = 15728;
+        published = true;
+      };
+
+      meshnets.yggdrasil = true;
+    };
+  };
+
+  systemd.services.i2pd.serviceConfig = {
+    DynamicUser = lib.mkForce false;
+    MemoryMax = "512M";
+  };
 
   networking.firewall.allowedTCPPorts = [
     17283
